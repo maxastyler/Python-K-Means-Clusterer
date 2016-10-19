9fb728716232dfcfb25c7af515ce03ccd249b3b9
Max Tyler <mtyler88@gmail.com>
2016-10-19 14:41:57 +0100

    Wrote clusterer script that can cluster a set of data into N means

---
+++ clusterer.py
@@ -0,0 +1,44 @@
+#Script for clustering some n-dimensional data by k-means
+
+from math import pow, sqrt
+from random import sample
+
+def distance(point1, point2):
+    return sqrt(sum(pow(point1[i]-point2[i], 2) for i in range(len(point1))))
+
+def average_points(list_of_points):
+    total=[0 for i in range(len(list_of_points[0]))]
+    for point in list_of_points:
+        for i in range(len(point)):
+            total[i]+=point[i]
+    for i in range(len(total)):
+        total[i]/=len(list_of_points)
+    return total
+
+#function returns a list of points which are the centers 
+def k_means(points, num_means, min_distance):
+    min_distance=float('inf')
+    above_distance=True
+    new_means=sample(points, num_means)   
+    mean_points={}
+    while above_distance:
+        for mean in new_means:
+            mean_points[mean]=[] 
+        above_distance=False
+        for point in points:
+            smallest_distance=float('inf')
+            smallest_mean=new_means[0]
+            for mean in new_means:
+                dist=distance(point, mean)
+                if dist < smallest_distance:
+                    smallest_distance=dist
+                    smallest_mean=mean
+            mean_points[smallest_mean].append(point)
+        old_means=new_means.copy()
+        new_means.clear()
+        for key in old_means: 
+            new_means.append(average_points(mean_points[key]))
+            if distance(key, new_means[-1])>=min_distance:
+                above_distance=True
+        mean_points.clear() 
+    return new_means
